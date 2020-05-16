9c67ed38db9db94d52ee92d92fa662f4ff3fd58a
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-17 00:04:35 +0900

    comment delete

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,18 +2,6 @@ $(function() {
   get_data();
 });
 
-// // ajaxの基本形
-// $.ajax({
-//   // web.app(routingしたURLを指定する)
-//   url: "result/ajax",
-//   dataType: "json",
-//   success: data=> {
-//     // 成功時の処理
-//   }
-//   error: () => {
-//     // エラー時の処理
-//   }
-// })
 
 function get_data() {
   $.ajax({
