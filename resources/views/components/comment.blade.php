d20a24c866cd59e83875f057c959188c8f0539ec
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-16 22:55:11 +0900

    typo

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,7 +1,7 @@
 <div class="media">
   <div class="media-body comment-body">
     <div class="row">
-      <span class="comment-body-user">{{$item->name}}/span>
+      <span class="comment-body-user">{{$item->name}}</span>
       <span class="comment-body-time">{{$item->create_at}}</span>
     </div>
     <span class="comment-body-content">{{!! nl2br(e($item->comment)) !!}}</span>
