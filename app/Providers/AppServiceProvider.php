9568fac39b00227ccf1437b76ba5f2f14ee94bed
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-17 18:04:15 +0900

    verchar191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\Schema;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,8 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      Schema::defaultStringLength(191);
       if (\App::environment('production')) {
           \URL::forceScheme('https');
       }
+
     }
 }
