7cc35abd57ed91effc18a3ceb7babcbe04ab5f75
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-17 19:51:01 +0900

    user cannt regist

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,9 +69,9 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        @if (Route::has('register'))
+                        <!-- @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
-                        @endif
+                        @endif -->
                     @endauth
                 </div>
             @endif
