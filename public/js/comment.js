135b8e9558ae6754518096502daff0683d40941e
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-05-17 12:33:03 +0900

    document set by js. json comunication display.

--- public/js/comment.js
+++ public/js/comment.js
@@ -8,10 +8,22 @@ function get_data() {
     url:"result/ajax/",
     dataType:"json",
     success:data =>{
+      $("#comment-data").find(".comment-visible").remove();
       for (var i = 0; i < data.comments.length; i++)
       {
-        console.log(data.comments[i].name);
-        console.log(data.comments[i].comment);
+        var html = `
+                  <div class="media comment-visible">
+                      <div class="media-body comment-body">
+                          <div class="row">
+                            <span class="comment-body-user" id="name">${data.comments[i].name}</span>
+                            <span class="comment-body-time" id="created_at">${data.comments[i].created_at}</span>
+                          </div>
+                          <span class="comment-body-content" id="comment">${data.comments[i].comment}</span>
+                      </div>
+                  </div>
+                  `;
+
+        $("#comment-data").append(html);
       }
     },
     error:()=>{
